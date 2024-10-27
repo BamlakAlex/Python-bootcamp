f88a0b370a955084890dcdf3db3e192994608d87
bamlakalex <bamlakalex00@gmail.com>
2024-10-27 14:10:28 -0700

    assignment two done

---
+++ Assignments/Assignment 2.py
@@ -0,0 +1,46 @@
+# Medium Level Challenge
+salary= (20, 30, 50)
+red,_,_ = salary
+_,blue,_ = salary
+_,_,green = salary
+print(red)
+print(blue)
+print(green)
+
+lists= [20,30,50]
+print(lists)
+
+x= lists[0]
+y= lists[1]
+z= lists[2]
+
+
+if x+y+z > 20 :
+ print("High")
+else: 
+ print("Low")
+
+# Hard Level Challenge
+
+rent=(10,21,30,43,50)
+blue,_,_,_,_= rent
+_,green,_,_,_= rent
+_,_,white,_,_= rent
+_,_,_,yellow,_=rent
+_,_,_,_,pink= rent
+
+print(blue)
+print(green)
+print(white)
+print(yellow)
+print(pink)
+
+odd= (21,43)
+
+y= list(odd)
+print(y)
+
+if len(y)>=3:
+ print("More odds")
+else:
+ print("Fewer odds") 
